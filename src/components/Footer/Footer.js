8c6f424a0f03b0eb35377be62ef05eb2db3aa9af
Lilianada <lilibest30.lb@gmail.com>
2022-04-06 23:21:51 +0100

    Creating the react app

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+export default function Footer () {
+
+    return(
+        <footer className="footerSection">
+            <div className="greeting"><p>Designslilian</p></div>
+            <div className="copyrightWhite"><p>© 2022 <strong>PersonalityCheck,</strong> inc.</p></div>
+        </footer>
+    )
+}
