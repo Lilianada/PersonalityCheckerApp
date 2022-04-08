d8dacbabd129bc9bf058349258d4bb4ccdfd4862
Lilianada <lilibest30.lb@gmail.com>
2022-04-08 09:57:17 +0100

    site meta data added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
 import Home from './pages/Home/Home';
+import DocumentMeta from "react-document-meta";
+
+const meta = {
+  title: "PersonalityCheck App",
+  description: "Take our Personality Test and get a “freakishly accurate” description of who you are and why you do things the way you do.",
+  canonical: "https://personalitychecker.netlify.app/",
+  meta: {
+    charset: "utf-8",
+    name: {
+      keywords: "personality,introvert,extrovert,test"
+    }
+  }
+};
 
 function App() {
   return (
     <div className="App bodyWrapper">
+      <DocumentMeta {...meta} />
       <Home/>
     </div>
   );
